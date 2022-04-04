2ff467cea65bd1ec30fc7b525ebd199fb0df64c0
suyatinglaura <e0492464@u.nus.edu>
2022-04-04 16:00:11 +0800

    first commit

---
+++ src/components/RandomNumber.vue
@@ -0,0 +1,46 @@
+<template>
+<h1>{{msg}}</h1>
+<button @click="genRanNum()">Generate random Number</button>
+<h2>{{random_number}}</h2>
+</template>
+
+<script>
+export default {
+  name: 'RandomNumber',
+
+  data(){
+    return{
+    random_number:0
+    }
+  },
+
+  props: {
+    msg: String,
+    min:{type: Number, default: 1},
+    max:{type: Number, default: 10}
+  },
+
+  methods:{
+    genRanNum(){
+      this.random_number = Math.floor(Math.random() * (this.max - this.min + 1) + this.min) // generate random number from 1 to 100
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,33 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import RandomNumber from "@/components/RandomNumber.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+describe("RandomNumber.vue", () => {
+  it("Default value is 0", () => {
+    const wrapper = shallowMount(RandomNumber);
+    expect(wrapper.html()).toContain(0);
+  });
+});
+
+describe("RandomNumber.vue", () => {
+  it("The random number is in the range of 1 to 10", async () => {
+    const wrapper = shallowMount(RandomNumber);
+    await wrapper.find('button').trigger('click')
+    const random_number = parseInt(wrapper.find('h2').text())
+    expect(random_number).toBeGreaterThanOrEqual(1)
+    expect(random_number).toBeLessThanOrEqual(10)
+  });
+});
+
+describe("RandomNumber.vue", () => {
+  it("Test the props", async () => {
+    const wrapper = shallowMount(RandomNumber,{
+      props:{
+        min:50,max:100
+      }
     });
-    expect(wrapper.text()).toMatch(msg);
+    await wrapper.find('button').trigger('click')
+    const random_number = parseInt(wrapper.find('h2').text())
+    expect(random_number).toBeGreaterThanOrEqual(50)
+    expect(random_number).toBeLessThanOrEqual(100)
   });
 });
